0cea0d616841598da64f6e4eca9cc8898f12b8b1
George Flores <georgeflores886@gmail.com>
2020-05-22 16:17:01 -0400

    Created ReadMe layout in generateMarkDown

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 //
 var fs = require("fs");
 var path = require("path");
-
+var inquirer = require("inquirer");//inquirer package
+var generate = require("./utils/generateMarkdown.js")
 
 //question prompts to user that generates the README
 const questions = [
     {
         type : "input",
         message:"What is your username?",
-        name: username
+        name: "username"
     }, 
     {
         type: "input",
@@ -27,37 +28,47 @@ const questions = [
     }, 
     {
         type: "input",
-        message: "Label your table of contents",
-        name:"tableContents"
-    },
-    {
-        type: "input",
-        message: "Explain the installation process",
-        name: "install"
+        message: "Command to run this project:",
+        name: "install",
+        default: "npm i"
     }, 
     {
-        type: "input",
+        type:"input",
+        message: "How to use this repo?",
+        name: "usage"
+    },
+    {
+        type: "list",
         message: "What license should this project have?",
-        name: "license"
+        name: "license",
+        choices: ["GIT", "Apache 2.0", "GPL", "BSD"]
     },
     {
         type: "input",
         message: "Who contributed to this project?",
         name: "contributer"
     }, 
+     {
+         type: "input",
+         message: "Command to run test?",
+         name: "test"
+     }
     {
         type: "input",
-        message: "Provide examples on how to run this project.",
-        name: "test"
+        message: "Antyhing the user needs to know abou running this repo?",
+        name: "misc"
     }
 ];
 
 function writeToFile(fileName, data) {
-
+    return fs.writeFile(fileName.path(process.cwd(data)));
 }
 
 function init() {
-
+    //launches questions prompt, then use feedback to pass to writetoFile function
+    inquirer.prompt(questions).then(function(answers) {
+        writeToFile("README.md", JSON.stringify(answers))
+    });
 }
 
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,9 +1,36 @@
+//creating the Markdown layout
 function generateMarkdown(data) {
-  
   return `
-# ${data.title}
+# ${data.project}
+  ${data.license}
+## Description 
+    ${data.description}
+## Table of Contents
+  *[Installation](#install)
+  *[Usage](#usage)
+  *[License](#license)
+  *[Contributors](#contributer)
+  
 
-`;
+## Installation
+
+  ${data.install}
+
+## Usage
+  ${data.usage}
+
+## License
+  ${data.license}
+
+## Contributors
+  ${data.contributing}
+
+## Tests
+  Run the following command: 
+  ${data.test}
+## Questions 
+Any questions or issues about this repo, contact at Github ${data.name} or via email ${data.email}.
+  `;
 }
 
 module.exports = generateMarkdown;
